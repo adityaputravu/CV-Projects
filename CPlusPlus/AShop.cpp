fc5c54fdb59e1ea5925d6ef6d8173abb72484d30
adityaputravu <32431279+adityaputravu@users.noreply.github.com>
2017-10-01 14:42:45 +0100

    Update and rename seven.cpp to AShop.cpp

---
+++ CPlusPlus/AShop.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string shopItemNames[3] = {
+                                "Snow Hat",
+                                "Hamster",
+                                "Dog"
+                               };
+
+    vector<string> itemNames;
+    itemNames.push_back("Snow Hat"); // Add to vector on the end.
+    itemNames.push_back("Scarf");
+    itemNames.push_back("Boxes");
+    itemNames.push_back("Rocks");
+
+    cout << itemNames.size() << endl;
+
+    itemNames[1] = itemNames.back();
+    itemNames.pop_back();
+
+    for (int i = 0; i < itemNames.size(); ++i) {
+        cout << itemNames[i] << endl;
+    }
+
+    return 0;
+}
